8e7d661412df14a0d6b13dd3e31148350a496588
agoesm <agus.miftah@ist.id>
2024-06-24 16:15:39 +0700

    Initial Commit

---
+++ CatchTheDuckGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheDuckGame
+//
+//  Created by Agus Miftah on 24/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
